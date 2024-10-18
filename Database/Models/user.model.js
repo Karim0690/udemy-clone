a1091370d6f45cd5ea14346d6c7460a627b38e1b
lamiaalotfy123 <lamiaalotfy123@gmail.com>
2024-10-18 08:44:33 +0300

    first &last name -languge

--- Database/Models/user.model.js
+++ Database/Models/user.model.js
@@ -1,77 +1,84 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-
-const userSchema = new mongoose.Schema(
-  {
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new mongoose.Schema({
     name: {
-      type: String,
-      required: true,
-      trim: true,
-      minLength: [2, 'too short name'],
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [2, "too short name"],
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [2, "too short name"],
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [2, "too short name"],
+    },
+    language: {
+
+
+
     },
     email: {
-      type: String,
-      trim: true,
-      required: [true, 'email is required'],
-      minLength: 1,
-      unique: [true, 'email must be unique'],
+        type: String,
+        trim: true,
+        required: [true, "email is required"],
+        minLength: 1,
+        unique: [true, "email must be unique"],
     },
     password: {
-      type: String,
-      require: true,
-      minLength: [8, "password can't be less than 8 characters"],
+        type: String,
+        require: true,
+        minLength: [8, "password can't be less than 8 characters"],
     },
     passwordChangedAt: Date,
     profilePic: String,
     role: {
-      type: [String],
-      enum: ['student', 'instructor', 'admin'],
-      default: ['student'],
+        type: [String],
+        enum: ["student", "instructor", "admin"],
+        default: ["student"],
     },
     isActive: {
-      type: Boolean,
-      default: true,
+        type: Boolean,
+        default: true,
     },
-    enrolledCourses: [
-      {
+    enrolledCourses: [{
         type: mongoose.Types.ObjectId,
-        ref: 'course',
-      },
-    ],
-    wishlist: [
-      {
+        ref: "course",
+    }, ],
+    wishlist: [{
         type: mongoose.Types.ObjectId,
-        ref: 'course',
-      },
-    ],
-    favorite: [
-      {
+        ref: "course",
+    }, ],
+    favorite: [{
         type: mongoose.Types.ObjectId,
-        ref: 'course',
-      },
-    ],
+        ref: "course",
+    }, ],
     headline: {
-      type: String,
-      trim: true,
+        type: String,
+        trim: true,
     },
     biography: {
-      type: String,
+        type: String,
     },
     social: {
-      website: String,
-      facebook: String,
-      linkedin: String,
-      youtube: String,
-      twitter: String,
+        website: String,
+        facebook: String,
+        linkedin: String,
+        youtube: String,
+        twitter: String,
     },
-  },
-  { timestamps: true, collection: 'User' }
-);
+}, { timestamps: true, collection: "User" });
 
-userSchema.pre('save', async function (next) {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
-export const userModel = mongoose.model('User', userSchema);
+export const userModel = mongoose.model("User", userSchema);
