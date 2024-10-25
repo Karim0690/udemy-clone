d25711b27bf04707e7d3b12d8648dcc907b7208c
Mahmoud AbdelRahem Fawzy <69515176+Mahmoud-Abdelrahem@users.noreply.github.com>
2024-10-25 14:47:28 +0300

    Update user.model.js

--- Database/Models/user.model.js
+++ Database/Models/user.model.js
@@ -7,19 +7,19 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      minLength: [2, 'too short name'],
+      minLength: [2, 'Too short name'],
     },
     email: {
       type: String,
       trim: true,
-      required: [true, 'email is required'],
+      required: [true, 'Email is required'],
       minLength: 1,
-      unique: [true, 'email must be unique'],
+      unique: [true, 'Email must be unique'],
     },
     password: {
       type: String,
-      require: true,
-      minLength: [8, "password can't be less than 8 characters"],
+      required: true,
+      minLength: [8, "Password can't be less than 8 characters"],
     },
     passwordChangedAt: Date,
     profilePic: String,
@@ -64,13 +64,19 @@ const userSchema = new mongoose.Schema(
       youtube: String,
       twitter: String,
     },
+    resetCode: {
+      type: String,
+      default: null, // Set default value to null
+    },
   },
   { timestamps: true, collection: 'User' }
 );
 
 userSchema.pre('save', async function (next) {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
   next();
 });
 
